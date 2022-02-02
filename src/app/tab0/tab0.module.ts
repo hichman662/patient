9377a65642e704473df73c6f127b3d90bb832689
Seyed Shahabadin Nasabeh <hichman662@gmail.com>
2022-02-02 12:00:50 +0100

    Home tab

---
+++ src/app/tab0/tab0.module.ts
@@ -0,0 +1,22 @@
+import { ExploreContainerComponentModule } from './../explore-container/explore-container.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { Tab0PageRoutingModule } from './tab0-routing.module';
+
+import { Tab0Page } from './tab0.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    Tab0PageRoutingModule,
+    ExploreContainerComponentModule
+  ],
+  declarations: [Tab0Page]
+})
+export class Tab0PageModule {}
