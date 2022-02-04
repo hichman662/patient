16ee5b5aded2b79ccbb8719b564ef1e8f4ef6635
Seyed Shahabadin Nasabeh <hichman662@gmail.com>
2022-02-04 11:58:22 +0100

    Sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { Component } from '@angular/core';
 import { Storage } from '@ionic/storage';
+import { Router, RouterEvent } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(public storage: Storage) {
+  active = '';
+
+  NAV = [
+    {
+      name: 'About',
+      link: '/nav/about',
+      icon: 'person-circle'
+    },
+    {
+      name: 'Logout',
+      link: '/login',
+      icon: 'exit'
+    }
+  ];
+
+  constructor(public storage: Storage,private router: Router) {
     this.storage.create();
-  }
+    this.router.events.subscribe((event: RouterEvent) => {
+      this.active = event.url;
+    });
+
+
+    }
 }
