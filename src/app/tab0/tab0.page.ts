48c471158101eebe76903a6c86ef907f74cd7cce
Seyed Shahabadin Nasabeh <hichman662@gmail.com>
2022-02-15 12:04:24 +0100

    toggle read message

--- src/app/tab0/tab0.page.ts
+++ src/app/tab0/tab0.page.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab0.page.scss'],
 })
 export class Tab0Page implements OnInit {
-
+  textByValue = false;
   constructor() { }
 
   ngOnInit() {
